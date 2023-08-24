994952ce72854989b714c6007e841e1096b4b1d7
Robin Burri <rburri@c1r16s5.42lausanne.ch>
2023-08-24 10:48:40 +0200

    working in container or without

--- app/main.py
+++ app/main.py
@@ -1,22 +1,28 @@
 import psycopg2
 from psycopg2 import OperationalError
+from retrying import retry
 
-try:
-    # Establish the connection
+@retry(wait_fixed=2000, stop_max_attempt_number=10)  # Wait 2 seconds between retries, try up to 10 times
+def connect_with_retry():
     conn = psycopg2.connect(
         database="docker_db",
         user="app",
         password="docker",
-        host="localhost",  # Use 'localhost' or the IP address of your database container
+        host="localhost",  # Use the service name as the hostname
         port="5432"
     )
+    return conn
 
+try:
+    # Attempt to connect with retry mechanism
+    db_connection = connect_with_retry()
+    print("Connected to the database!")
     # Create a cursor
-    cur = conn.cursor()
+    cur = db_connection.cursor()
 
     # Execute a statement
     cur.execute("SELECT version()")
-    
+
     # Fetch the result
     db_version = cur.fetchone()[0]
     print("Connected to PostgreSQL version:", db_version)
@@ -25,7 +31,6 @@ except OperationalError as e:
     print("Error:", e)
 finally:
     # Close the cursor and connection (regardless of success or failure)
-    if cur:
-        cur.close()
-    if conn:
-        conn.close()
+    cur.close()
+    db_connection.close()
+    
